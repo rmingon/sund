e5f5adca91ebe4db6cc2e013111d6dc540db0d89
rmingon <ronanmingon69550@gmail.com>
2022-09-28 05:50:47 +0200

    add font

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-gradient-to-b from-sky-500">
+    <TheHeader />
+    <Wave/> 
+    <Me/>
   </div>
 </template>
+
+<style>
+  
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+
+  html {
+    background-color: #F2F2F2;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+
+</style>
