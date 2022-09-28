a17b64966e44a3faea0f2e6659e914e6a6cf2f59
rmingon <ronanmingon69550@gmail.com>
2022-09-28 05:50:32 +0200

    tailwind + logo

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'sun': '#F2E205',
+        'sand': '#F2CB05',
+        'sand-shadow': '#A68B03',
+        'sand-light': '#F2E8B6',
+        'sun-light': '#F2F2F2'
+      },
+    },
+  },
+  plugins: [],
+}
