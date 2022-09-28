f35daf7387ae888e7ab48da12f3328942944ddba
rmingon <ronanmingon69550@gmail.com>
2022-09-28 05:51:47 +0200

    add top menu

---
+++ components/TheMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-row justify-around w-full items-start px-20 pt-3">
+    <div class="text-xl cursor-pointer hover:border-b-2 duration-100 hover:border-sand">Me</div>
+    <div class="text-xl cursor-pointer hover:border-b-2 duration-100 hover:border-sand">Stack</div>
+    <div class="flex flex-col justify-center items-center sticky top-0">
+      <div class="rounded-full drop-shadow-[0px_0px_12px_rgba(242,226,5,1)] mb-3">
+        <img width="60" src="../assets/logo.png" alt="">
+      </div>
+      <div class="text-2xl">
+        Sund studio
+      </div>
+    </div>
+    <div class="text-xl cursor-pointer hover:border-b-2 duration-100 hover:border-sand">Creations</div>
+    <div class="text-xl cursor-pointer hover:border-b-2 duration-100 hover:border-sand">Contact</div>
+  </div>
+</template>
